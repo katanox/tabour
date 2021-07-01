df91d50a52d9f9fa6ad85c369f6decdaa33c013e
Mantas <m.gavenavicius@katanox.com>
2021-07-01 17:13:08 +0300

    include message group id in protoeventpublisher

--- src/main/kotlin/com/katanox/tabour/core/proto/ProtoEventConsumer.kt
+++ src/main/kotlin/com/katanox/tabour/core/proto/ProtoEventConsumer.kt
@@ -1,11 +1,11 @@
-package com.katanox.tabour.core
+package com.katanox.tabour.core.proto
 
 import com.google.protobuf.Message
-import com.google.protobuf.MessageOrBuilder
 import com.google.protobuf.util.JsonFormat
+import com.katanox.tabour.core.EventConsumer
 
 abstract class ProtoEventConsumer<T: Message.Builder>: EventConsumer() {
-    fun consume(message: String, event: T): T {
+    fun parseMessageToEvent(message: String, event: T): T {
         JsonFormat.parser().merge(message, event)
         return event
     }
---
+++ src/main/kotlin/com/katanox/tabour/core/proto/ProtoEventPublisher.kt
@@ -0,0 +1,12 @@
+package com.katanox.tabour.core.proto
+
+import com.google.protobuf.MessageOrBuilder
+import com.google.protobuf.util.JsonFormat
+import com.katanox.tabour.core.EventPublisher
+
+abstract class ProtoEventPublisher: EventPublisher() {
+    fun publish(event: MessageOrBuilder, busUrl: String, messageGroupId: String? = null) {
+        val json: String = JsonFormat.printer().print(event)
+        this.publish(json, busUrl, messageGroupId)
+    }
+}
