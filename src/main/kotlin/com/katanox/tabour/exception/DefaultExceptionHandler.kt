1d97336b32c3a5f0699a5c150a2bb438d2e7353b
Mantas <m.gavenavicius@katanox.com>
2021-10-20 12:16:59 +0200

    remove redundant curly braces

--- src/main/kotlin/com/katanox/tabour/exception/DefaultExceptionHandler.kt
+++ src/main/kotlin/com/katanox/tabour/exception/DefaultExceptionHandler.kt
@@ -11,7 +11,7 @@ class DefaultExceptionHandler : ExceptionHandler {
 
     override fun handleException(message: Message, e: Exception): ExceptionHandler.ExceptionHandlerDecision {
         logger.error(
-            "error while processing message ${message.messageId} - message has not been deleted from SQS and will be retried:  ${e}",
+            "error while processing message ${message.messageId} - message has not been deleted from SQS and will be retried:  $e",
         )
         return ExceptionHandler.ExceptionHandlerDecision.RETRY
     }
