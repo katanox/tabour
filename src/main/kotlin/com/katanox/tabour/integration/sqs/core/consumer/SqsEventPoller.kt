c98e2696e9d76d945ab00ff5ea03014bee8b6a13
gpopides <g.popides@katanox.com>
2021-12-25 11:21:27 +0200

    use channels

--- src/main/kotlin/com/katanox/tabour/integration/sqs/core/consumer/SqsEventPoller.kt
+++ src/main/kotlin/com/katanox/tabour/integration/sqs/core/consumer/SqsEventPoller.kt
@@ -3,6 +3,9 @@ package com.katanox.tabour.integration.sqs.core.consumer
 import com.katanox.tabour.config.EventPollerProperties
 import com.katanox.tabour.exception.ExceptionHandler
 import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.channels.SendChannel
 import mu.KotlinLogging
 import software.amazon.awssdk.services.sqs.SqsAsyncClient
 import software.amazon.awssdk.services.sqs.model.DeleteMessageBatchRequest
@@ -36,9 +39,16 @@ class SqsEventPoller(
     fun start() = launch(Dispatchers.IO) {
         logger.info("starting SqsMessagePoller ${LocalDateTime.now()}")
 
+        val channel = Channel<List<Message>>()
+
         repeat(100) {
             launch {
-                pollMessages(::handleMessagesPack)
+                pollMessages(channel)
+            }
+        }
+        repeat(10) {
+            launch {
+                consumeMes(channel)
             }
         }
     }
@@ -48,10 +58,10 @@ class SqsEventPoller(
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
-    private suspend fun pollMessages(handler: (List<Message>) -> Unit) {
+    private suspend fun pollMessages(channel: SendChannel<List<Message>>) {
         while (true) {
             try {
-                handler(eventFetcher.fetchMessages(queueUrl))
+                channel.send(eventFetcher.fetchMessages(queueUrl))
             } catch (e: Exception) {
                 logger.error("error fetching messages from queue $eventHandler.sqsQueueUrl : ${e.message}")
             }
@@ -60,34 +70,38 @@ class SqsEventPoller(
         }
     }
 
-//    @OptIn(ExperimentalCoroutinesApi::class)
-//    private suspend fun consumeMes(channel: ReceiveChannel<List<Message>>, acknowledgeChannel: SendChannel<List<Message>>) {
-//    }
+    @OptIn(ExperimentalCoroutinesApi::class)
+    private suspend fun consumeMes(channel: ReceiveChannel<List<Message>>) {
+        coroutineScope {
+            for (pack in channel) {
+                handleMessagesPack(pack)
+            }
+        }
+    }
 
     private fun handleMessagesPack(sqsMessages: List<Message>) {
         val toBeAcknowledged = mutableListOf<Message>()
-        sqsMessages.parallelStream()
-            .forEach { sqsMessage ->
+        sqsMessages.forEach { sqsMessage ->
 //            logger.info("Received message ID {}", sqsMessage.messageId())
-                val message = sqsMessage.body()
-                try {
-                    eventHandler.onBeforeHandle(message)
-                    eventHandler.handle(message)
-                    toBeAcknowledged.add(sqsMessage)
+            val message = sqsMessage.body()
+            try {
+                eventHandler.onBeforeHandle(message)
+                eventHandler.handle(message)
+                toBeAcknowledged.add(sqsMessage)
 //            logger.debug(
 //                "message {} processed successfully - message has been deleted from SQS",
 //                sqsMessage.messageId()
 //            )
-                } catch (e: Exception) {
-                    when (exceptionHandler.handleException(sqsMessage, e)) {
-                        ExceptionHandler.ExceptionHandlerDecision.RETRY -> {}
-                        ExceptionHandler.ExceptionHandlerDecision.DELETE -> toBeAcknowledged.add(sqsMessage)
-                    }
-                } finally {
-                    acknowledgeMessages(toBeAcknowledged)
-                    eventHandler.onAfterHandle(message)
+            } catch (e: Exception) {
+                when (exceptionHandler.handleException(sqsMessage, e)) {
+                    ExceptionHandler.ExceptionHandlerDecision.RETRY -> {}
+                    ExceptionHandler.ExceptionHandlerDecision.DELETE -> toBeAcknowledged.add(sqsMessage)
                 }
+            } finally {
+                acknowledgeMessages(toBeAcknowledged)
+                eventHandler.onAfterHandle(message)
             }
+        }
     }
 
     private fun acknowledgeMessages(messages: List<Message>) {
