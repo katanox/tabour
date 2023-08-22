5f86591e9c5111ac92764766af53b112f90cc755
gpopides <g.popides@katanox.com>
2023-08-22 10:10:34 +0200

    clean up

--- core/src/main/kotlin/com/katanox/tabour/sqs/SqsRegistry.kt
+++ core/src/main/kotlin/com/katanox/tabour/sqs/SqsRegistry.kt
@@ -42,22 +42,14 @@ internal constructor(
 
     /** Adds a collection of consumers to the registry */
     fun addConsumers(consumers: List<SqsConsumer>): SqsRegistry<T> =
-        this.apply {
-            consumers.fold(this) { registry, consumer ->
-                registry.apply { this.addConsumer(consumer) }
-            }
-        }
+        this.apply { consumers.fold(this) { registry, consumer -> registry.addConsumer(consumer) } }
 
     fun <K> addProducer(producer: SqsProducer<K>): SqsRegistry<T> =
         this.apply { producers.add(producer) }
 
     /** Adds a collection of producers to the registry */
     fun <K> addProducers(producers: List<SqsProducer<K>>): SqsRegistry<T> =
-        this.apply {
-            producers.fold(this) { registry, producer ->
-                registry.apply { this.addProducer(producer) }
-            }
-        }
+        this.apply { producers.fold(this) { registry, producer -> registry.addProducer(producer) } }
 
     /**
      * Starts the consuming process using the Consumers that have been registered up until the time
