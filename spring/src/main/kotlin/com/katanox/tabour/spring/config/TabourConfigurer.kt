0e4067b83e50b544adecb42b3b87ca026bf8a85c
gpopides <46693847+gpopides@users.noreply.github.com>
2023-09-15 14:31:28 +0300

    Fix spring package (#281)

--- spring/src/main/kotlin/com/katanox/tabour/spring/config/TabourConfigurer.kt
+++ spring/src/main/kotlin/com/katanox/tabour/spring/config/TabourConfigurer.kt
@@ -6,66 +6,71 @@ import com.katanox.tabour.configuration.core.tabour
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import org.springframework.boot.autoconfigure.AutoConfiguration
-import org.springframework.context.ConfigurableApplicationContext
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.ApplicationContext
+import org.springframework.context.ApplicationListener
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Lazy
+import org.springframework.context.event.ContextRefreshedEvent
+import org.springframework.stereotype.Component
 import org.springframework.util.ClassUtils
 
 val scope = CoroutineScope(Dispatchers.IO)
 
-@AutoConfiguration
-open class TabourConfigurer {
+class TabourConfigurer
 
-    @Bean
-    @Lazy(false)
-    open fun setupTabour(context: ConfigurableApplicationContext): Tabour {
+@Component
+class ContextRefreshedEventListener(
+    @Value("\${tabour.config.num-of-threads:2}") val threadsCount: Int
+) : ApplicationListener<ContextRefreshedEvent?> {
+    override fun onApplicationEvent(contextRefreshedEvent: ContextRefreshedEvent?) {
+        if (contextRefreshedEvent?.applicationContext != null) {
+            setupTabour(contextRefreshedEvent.applicationContext)
+        }
+    }
+
+    @Bean @Lazy(false) fun tabourBean(): Tabour = tabour { this.numOfThreads = threadsCount }
+
+    private fun setupTabour(context: ApplicationContext) {
         val annotatedBeans: Map<String, Any> =
             context.getBeansWithAnnotation(AutoconfigureTabour::class.java)
 
         // only one class should be annotated with this annotation
-        return if (annotatedBeans.isNotEmpty() && annotatedBeans.size == 1) {
+        if (annotatedBeans.isNotEmpty() && annotatedBeans.size == 1) {
             val mainClass = ClassUtils.getUserClass(annotatedBeans.values.first())
 
-            val beanFactory = context.beanFactory
+            val tabourContainers = context.getBeansOfType(Tabour::class.java)
 
-            val threads =
-                context.environment.getProperty("tabour.config.num-of-threads", Int::class.java)
-                    ?: 2
-
-            constructTabourContainer(mainClass, threads) {
-                beanFactory.getBeansOfType(Registry::class.java).values.toList()
+            if (tabourContainers.size == 1) {
+                launchTabour(mainClass, tabourContainers.values.first()) {
+                    context.getBeansOfType(Registry::class.java).values.toList()
+                }
             }
-        } else {
-            tabourBean(emptyList(), 1)
         }
     }
 }
 
-internal fun <T> constructTabourContainer(
-    c: Class<T>,
-    numOfThreads: Int = 1,
+/**
+ * starts the tabour container only if the annotation [AutoconfigureTabour] is present and there are
+ * [Registry] instances available in the spring context
+ */
+internal fun launchTabour(
+    mainClass: Class<*>,
+    tabourContainer: Tabour,
     registriesProvider: () -> List<Registry<*>>
-): Tabour {
-    val annotation = c.getAnnotation(AutoconfigureTabour::class.java)
+) {
+    val annotation = mainClass.getAnnotation(AutoconfigureTabour::class.java)
 
-    return if (annotation != null) {
-        tabourBean(registriesProvider(), numOfThreads)
-    } else {
-        tabourBean(emptyList(), numOfThreads)
+    if (annotation != null) {
+        val registries = registriesProvider()
+
+        if (registries.isNotEmpty()) {
+            registries
+                .fold(tabourContainer) { container, registry -> container.register(registry) }
+                .apply {
+                    val container = this
+                    scope.launch { container.start() }
+                }
+        }
     }
-}
-
-private fun tabourBean(registries: List<Registry<*>>, threads: Int): Tabour {
-    val container = tabour { this.numOfThreads = threads }
-
-    registries.fold(container) { tabourContainer, registry ->
-        tabourContainer.apply { this.register(registry) }
-    }
-
-    if (registries.isNotEmpty()) {
-        scope.launch { container.start() }
-    }
-
-    return container
 }
