b7e27b004a0dc71ce7b7382b8ccc9d2d60a90c4e
gpopides <46693847+gpopides@users.noreply.github.com>
2024-07-18 10:33:39 +0200

    Publish to central (#284)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,11 @@
+import java.net.URI
+
 plugins {
     kotlin("jvm") version "1.9.22"
     `maven-publish`
     id("com.ncorti.ktfmt.gradle") version "0.17.0"
     id("org.jetbrains.dokka") version "1.9.10"
+    signing
 }
 
 val tabourVersion = rootProject.file("version").readText().trim()
@@ -14,6 +17,7 @@ subprojects {
     apply(plugin = "maven-publish")
     apply(plugin = "com.ncorti.ktfmt.gradle")
     apply(plugin = "org.jetbrains.dokka")
+    apply(plugin = "signing")
 
     version = tabourVersion
 
@@ -23,20 +27,64 @@ subprojects {
 
     kotlin { jvmToolchain(21) }
 
+    java {
+        withJavadocJar()
+        withSourcesJar()
+    }
+
     publishing {
         publications {
             repositories {
                 maven {
-                    name = "GitHubPackages"
-                    url = uri("https://maven.pkg.github.com/katanox/tabour")
-                    credentials {
-                        username = "gpopides"
-                        password = System.getenv("TABOUR_TOKEN")
+                    url = URI.create("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+                    authentication {
+                        credentials {
+                            username = System.getenv("OSSRH_USERNAME")
+                                ?.ifEmpty { throw RuntimeException("OSSRH_USERNAME environment variable is not set") }
+                                ?: throw RuntimeException("OSSRH_USERNAME environment variable is not set")
+                            password = System.getenv("OSSRH_PASSWORD")
+                                ?.ifEmpty { throw RuntimeException("OSSHR_PASSWORD environment variable is not set") }
+                                ?: throw RuntimeException("OSSHR_PASSWORD  environment variable is not set")
+                        }
+                    }
+
+                }
+            }
+            publications {
+                register<MavenPublication>("gpr") {
+                    from(components["java"])
+
+                    pom {
+                        name = "Tabour"
+                        description = "A Kotlin library to consume and produce SQS messages"
+                        url = "https://github.com/katanox/tabour"
+                        groupId = "com.katanox.tabour"
+                        version = tabourVersion
+
+                        licenses {
+                            license {
+                                name = "The Apache License, Version 2.0"
+                                url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+                            }
+                        }
+                        developers {
+                            developer {
+                                id = "katanoxdevs"
+                                name = "Katanox Developers"
+                                email = "katanox@developers.com"
+                            }
+                        }
+                        scm {
+                            url = "https://github.com/katanox/tabour"
+                        }
                     }
                 }
             }
-            publications { register<MavenPublication>("gpr") { from(components["java"]) } }
         }
+    }
+
+    signing {
+        sign(publishing.publications["gpr"])
     }
 
     ktfmt { kotlinLangStyle() }
