f7e04a23335736f0751609e459587c80c50e2faa
gpopides <mail@gpopides.me>
2025-03-08 17:44:15 +0100

    Change plugin attributes
    Signed-off-by: gpopides <mail@gpopides.me>

--- ktor/src/main/kotlin/com/katanox/tabour/ktor/TabourPlugin.kt
+++ ktor/src/main/kotlin/com/katanox/tabour/ktor/TabourPlugin.kt
@@ -1,7 +1,5 @@
 package com.katanox.tabour.ktor
 
-import com.katanox.tabour.Tabour
-import com.katanox.tabour.configuration.Registry
 import com.katanox.tabour.configuration.core.tabour
 import io.ktor.server.application.ApplicationStarted
 import io.ktor.server.application.ApplicationStopped
@@ -13,38 +11,27 @@ import io.ktor.utils.io.locks.ReentrantLock
 import io.ktor.utils.io.locks.withLock
 
 class TabourConfiguration {
-    var registries: List<Registry<*>> = emptyList()
-    var numOfThreads: Int = 1
+    var tabour = tabour {}
     var enabled: Boolean = false
 }
 
 @OptIn(InternalAPI::class)
 val TabourPlugin =
     createApplicationPlugin(name = "TabourPlugin", createConfiguration = ::TabourConfiguration) {
-        var t: Tabour? = null
         val lock = ReentrantLock()
 
         on(MonitoringEvent(ApplicationStarted)) { application ->
             if (pluginConfig.enabled) {
                 application.log.info("Starting tabour")
-
-                lock.withLock {
-                    val tb =
-                        tabour { numOfThreads = pluginConfig.numOfThreads }
-                            .apply { pluginConfig.registries.forEach { register(it) } }
-                    t = tb
-                    t?.start()
-                }
+                lock.withLock { pluginConfig.tabour.start() }
             }
         }
         on(MonitoringEvent(ApplicationStopped)) { application ->
             if (pluginConfig.enabled) {
                 application.log.info("Stopping tabour")
 
-                val tb = t
-
-                if (pluginConfig.enabled && tb != null) {
-                    lock.withLock { tb.stop() }
+                if (pluginConfig.enabled) {
+                    lock.withLock { pluginConfig.tabour.stop() }
                 }
 
                 application.monitor.unsubscribe(ApplicationStarted) {}
