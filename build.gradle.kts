1db5d80586db67b4b856f51640515e0216c75b32
gpopides <g.popides@katanox.com>
2025-03-21 09:47:31 +0100

    remove plug, enable publishing to nexus via gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,83 +1,100 @@
 import java.net.URI
 
 plugins {
-  kotlin("jvm").version(libs.versions.kotlin)
-  `maven-publish`
-  alias(libs.plugins.dokka)
-  alias(libs.plugins.ktfmt)
-  signing
+    kotlin("jvm").version(libs.versions.kotlin)
+    `maven-publish`
+    alias(libs.plugins.dokka)
+    alias(libs.plugins.ktfmt)
+    alias(libs.plugins.nexus.publish)
+    signing
 }
 
 val tabourVersion = rootProject.file("version").readText().trim()
 
 repositories { mavenCentral() }
 
+group = "com.katanox.tabour"
+
 subprojects {
-  apply(plugin = "org.jetbrains.kotlin.jvm")
-  apply(plugin = "maven-publish")
-  apply(plugin = "com.ncorti.ktfmt.gradle")
-  apply(plugin = "org.jetbrains.dokka")
-  apply(plugin = "signing")
+    plugins.apply("org.jetbrains.kotlin.jvm")
+    plugins.apply("maven-publish")
+    plugins.apply("com.ncorti.ktfmt.gradle")
+    plugins.apply("org.jetbrains.dokka")
+    plugins.apply("signing")
 
-  version = tabourVersion
+    version = tabourVersion
 
-  repositories { mavenCentral() }
+    repositories { mavenCentral() }
 
-  kotlin { jvmToolchain(21) }
+    kotlin { jvmToolchain(21) }
 
-  java {
-    withJavadocJar()
-    withSourcesJar()
-  }
-
-  publishing {
-    publications {
-      repositories {
-        maven {
-          url = URI.create("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
-          authentication {
-            credentials {
-              username = System.getenv("OSSRH_USERNAME")
-              password = System.getenv("OSSRH_PASSWORD")
-            }
-          }
-        }
-      }
-
-      publications {
-        register<MavenPublication>("gpr") {
-          from(components["java"])
-
-          pom {
-            name = "Tabour"
-            description = "A Kotlin library to consume and produce SQS messages"
-            url = "https://github.com/katanox/tabour"
-            groupId = "com.katanox.tabour"
-            version = tabourVersion
-
-            licenses {
-              license {
-                name = "The Apache License, Version 2.0"
-                url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
-              }
-            }
-            developers {
-              developer {
-                id = "katanoxdevs"
-                name = "Katanox Developers"
-                email = "developers@katanox.com"
-              }
-            }
-            scm { url = "https://github.com/katanox/tabour" }
-          }
-        }
-      }
+    java {
+        withJavadocJar()
+        withSourcesJar()
     }
-  }
 
-  signing { sign(publishing.publications["gpr"]) }
+    publishing {
+        publications {
+            repositories {
+                maven {
+                    url =
+                        URI.create(
+                            "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
+                        )
+                    authentication {
+                        credentials {
+                            username = System.getenv("OSSRH_USERNAME")
+                            password = System.getenv("OSSRH_PASSWORD")
+                        }
+                    }
+                }
+            }
 
-  ktfmt { kotlinLangStyle() }
+            publications {
+                register<MavenPublication>("gpr") {
+                    from(components["java"])
 
-  tasks.test { useJUnitPlatform() }
+                    pom {
+                        name = "Tabour"
+                        description = "A Kotlin library to consume and produce SQS messages"
+                        url = "https://github.com/katanox/tabour"
+                        groupId = "com.katanox.tabour"
+                        version = tabourVersion
+
+                        licenses {
+                            license {
+                                name = "The Apache License, Version 2.0"
+                                url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+                            }
+                        }
+                        developers {
+                            developer {
+                                id = "katanoxdevs"
+                                name = "Katanox Developers"
+                                email = "developers@katanox.com"
+                            }
+                        }
+                        scm { url = "https://github.com/katanox/tabour" }
+                    }
+                }
+            }
+        }
+    }
+
+    signing { sign(publishing.publications["gpr"]) }
+
+    ktfmt { kotlinLangStyle() }
+
+    tasks.test { useJUnitPlatform() }
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(
+                uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+            )
+        }
+    }
 }
